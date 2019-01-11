972097db1c8d1891716f995d7dec352c094ac4d8
Gabriel Barbu <gabriel.barbu@iquestgroup.com>
2019-01-12 00:53:30 +0200

    added prerequisites install

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ $urlOfTheme = "https://raw.githubusercontent.com/bibistroc/ConEmu-environment/ma
 if(($PSVersionTable.PSVersion.Major) -lt 3) {
     Write-Output "PowerShell 3 or greater is required to use this profile."
     Write-Output "Upgrade PowerShell: https://docs.microsoft.com/en-us/powershell/scripting/setup/installing-windows-powershell"
-    break
+    exit
 }
 
 
@@ -14,7 +14,73 @@ if((Get-ExecutionPolicy) -gt 'RemoteSigned' -or (Get-ExecutionPolicy) -eq 'ByPas
     Write-Output "PowerShell requires an execution policy of 'RemoteSigned' to configure this profile."
     Write-Output "To make this change please run:"
     Write-Output "'Set-ExecutionPolicy RemoteSigned -scope CurrentUser'"
-    break
+    exit
+}
+
+function Show-Exit {
+    Write-Host "Press any key to exit..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    exit
+}
+
+function Install-Conemu {
+	Write-Host "This script is not run from ConEmu."
+	Write-Host "If you have ConEmu installed, quit now and run this script from it."
+	$installConemu = Read-Host -Prompt "Do you want to install ConEmu? [Y/q]"
+	if ($installConemu -like "y" -or $installConemu -eq "") {
+        # Installation script from https://conemu.github.io/en/AutoInstall.html
+        Start-Process powershell `
+            -ArgumentList "-Command iex ((new-object net.webclient).DownloadString('https://conemu.github.io/install.ps1'))" `
+            -Wait
+
+        Write-Host "ConEmu installed"
+        Write-Host "You can start ConEmu and run this script again from it to continue."
+        Show-Exit
+	} else {
+        Write-Host "ConEmu is required for this to work properly."
+        Write-Host "If you want to use it, you can run this script again."
+        Show-Exit
+    }
+}
+
+function Install-Git {
+    Write-Host "Git is not installed."
+    $installGit = Read-Host -Prompt "Do you want to install it? [Y/n]"
+    if ($installGit -like "y" -or $installGit -eq "") {
+        Write-Host "In order to install git, an external script will be run."
+        Write-Host "If you want to inspect the script, you can find it here: "
+        Write-Host "http://r.gbarbu.eu/installgit"
+        Write-Host "Press [ENTER] to continue or [CTRL+C] to cancel."
+        Read-Host
+        Write-Host "Starting installing"
+        Start-Process powershell `
+            -ArgumentList "-Command iwr r.gbarbu.eu/installgit -UseBasicParsing | iex" `
+            -Wait `
+            -Verb RunAs
+
+        Write-Host "Git installed"
+
+        # Reset environment variables
+        $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + 
+                    [System.Environment]::GetEnvironmentVariable("Path","User")
+    } else {
+        Write-Host "Git is required for this to work properly."
+        Write-Host "If you want to use it, you can run this script again."
+        Show-Exit 
+    }
+}
+
+# check for prerequisites
+try {
+	IsConEmu | Out-Null
+} catch {
+	Install-Conemu
+}
+
+try {
+    git | Out-Null
+} catch {
+    Install-Git
 }
 
 # checking for required powershell modules
---
+++ installGit.ps1
@@ -0,0 +1,47 @@
+$installerLocation = "https://github.com/git-for-windows/git/releases/latest/"
+
+# Check for powershell version
+if(($PSVersionTable.PSVersion.Major) -lt 3) {
+    Write-Output "PowerShell 3 or greater is required to use this script to install git."
+    Write-Output "Upgrade PowerShell: https://docs.microsoft.com/en-us/powershell/scripting/setup/installing-windows-powershell"
+    break
+}
+
+# Detect architecture
+$envArch = $ENV:PROCESSOR_ARCHITECTURE
+$detectedArch
+
+switch ($envArch) {
+    { @("AMD64", "IA64") -contains $_ } {
+        $detectedArch = "64"
+    }
+    Default {
+        $detectedArch = "86"
+    }
+}
+Write-Host "Detected $detectedArch architecture"
+
+# Get installer url
+$artifactLinks = (Invoke-WebRequest -Uri $installerLocation -UseBasicParsing).Links
+$artifactSearchString = "*/git-*$detectedArch-bit.exe"
+$artifactUrl = ($artifactLinks | Where-Object { $_.href -like $artifactSearchString }).href
+$artifactDownloadUrl = "https://github.com/$artifactUrl"
+
+# Download installer
+Write-Host "Downloading git installer. Please wait ..."
+$artifactDownloadPath = Join-Path $env:TEMP "gitinstaller.exe"
+$installerWebClient = New-Object System.Net.WebClient
+$installerWebClient.DownloadFile($artifactDownloadUrl, $artifactDownloadPath)
+Write-Host "Downloaded git installer into: $artifactDownloadPath"
+
+Write-Host "Installing git. This will take a while. Please wait"
+Start-Process -FilePath "$artifactDownloadPath" -ArgumentList "/VERYSILENT","/SUPPRESSMSGBOXES","/NORESTART","/NOCANCEL","/SP-","/LOG","/GitAndUnixToolsOnPath","/NoAutoCrlf" -Wait
+Write-Host "Installation finished."
+
+# Reset environment variables
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + 
+            [System.Environment]::GetEnvironmentVariable("Path","User")
+
+Write-Host "Script finished. You can start using git."
+Write-Host "Press any key to exit"
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
